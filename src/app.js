af2662ca70820dbb740cb823f42af27fdc0ca6b7
Tara Popov <tarapopov6@gmail.com>
2026-07-19 14:43:06 +0200

    Ciscenje greskom dupliranih fajlova

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require("express"); //ucitavanje biblioteke
 
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express(); //kreiranje express aplikacije
 
 app.use(express.json()); //prihvati json podatke
 
 module.exports = app; //omogucava da server koristi aplikaciju
+
--- veb_aplikacija/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./src/app"); //ucitavanje aplikacije
-
-const PORT = 3000; //definisanje porta na kojem ce server raditi
-
-app.listen(PORT, () => { //pokretanje servera na definisanom portu
-    console.log("Server radi na portu 3000");
-});
