a680c10491c5db0e3c4c96881ad332338a43a66b
Tamara Obradovic <obradoviic.t@gmail.com>
2026-08-24 14:53:36 +0200

    Dodati Group i Expense modeli sa migracijama i relacijama

---
+++ app/Models/Expense.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+        protected $fillable = ['description', 'amount', 'group_id', 'paid_by'];
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function payer()
+    {
+        return $this->belongsTo(User::class, 'paid_by');
+    }
+}
+
